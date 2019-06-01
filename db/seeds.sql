7d3dc02b39ecc12fb9348207388e2faffb03ea21
stravis11 <stevetravis11@gmail.com>
2019-06-01 08:08:22 -0400

    created schema and source, db set up

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('Big Mac', false);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('Cheeseburger', true);
+INSERT INTO burger
+    (burger_name, devoured)
+VALUES
+    ('Whopper', true);
+
